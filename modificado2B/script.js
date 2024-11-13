62cf5d673aa9c24614e545e99c7f4a9cadd1f495
Ricardo Almeida Branco <133051236+ProfRicardoBranco@users.noreply.github.com>
2024-11-13 08:31:34 -0300

    modificado2B/index.html

--- modificado2B/script.js
+++ modificado2B/script.js
@@ -1,21 +1,21 @@
-// Função para abrir o vídeo flutuante
+// Função para abrir o vídeo no player flutuante
 function abrirVideo(videoId) {
     const player = document.getElementById('player');
     const videoFlutuante = document.getElementById('videoFlutuante');
 
-    // Atualiza a URL do iframe para o vídeo do YouTube
+    // Atualiza o iframe com o URL do vídeo
     player.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
 
     // Exibe o player flutuante
     videoFlutuante.style.display = 'block';
 }
 
-// Função para fechar o vídeo flutuante
+// Função para fechar o player flutuante
 function fecharVideo() {
     const player = document.getElementById('player');
     const videoFlutuante = document.getElementById('videoFlutuante');
-    
-    // Limpa o src do iframe para parar o vídeo
+
+    // Limpa o vídeo para parar a reprodução ao fechar
     player.src = '';
 
     // Esconde o player flutuante
